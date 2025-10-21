f33cfb4a2af572ab92e2ed60a4e605b54763f779
KatyChagay <chagaikat@gmail.com>
2025-10-21 16:11:28 +0300

    Fix after review 2.0

--- conftest.py
+++ conftest.py
@@ -1,21 +1,7 @@
 import pytest
 
-from .helpers import (
-    create_courier,
-    get_courier_id,
-    delete_courier,
-    generate_order_payload,
-    generate_courier_data
-)
+from .helpers import generate_order_payload
 
-@pytest.fixture
-def created_courier():
-    courier_data = generate_courier_data()  
-    resp = create_courier(courier_data)
-    yield courier_data, resp
-    courier_id = get_courier_id(courier_data["login"], courier_data["password"])
-    if courier_id:
-        delete_courier(courier_id)
 
 @pytest.fixture
 def order_payload():
